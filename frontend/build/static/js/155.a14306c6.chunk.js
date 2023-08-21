010a2ca7250af385f0ee051fa6744cc4a9300d01
Rajat Sardesai <rajatsardesai@gmail.com>
2023-08-21 11:03:22 +0530

    fix: add build folder

---
+++ frontend/build/static/js/155.a14306c6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhekto=self.webpackChunkhekto||[]).push([[155],{5155:function(e,n,t){t.r(n);var u=t(2791),s=t(9434),r=t(7689),i=t(2148),f=t(184);n.default=function(e){var n=e.isAdmin,t=(0,r.s0)(),c=(0,s.v9)((function(e){return e.user})),a=c.isAuthenticated,o=c.loading,h=c.user;return(0,u.useEffect)((function(){if(!o&&a&&!n&&!h)return(0,f.jsx)(i.Z,{})}),[n,o,t,a,h]),(!n||h&&"admin"===h.role)&&(n||a)?(0,f.jsx)(r.j3,{}):(0,f.jsx)(i.Z,{})}}}]);
+//# sourceMappingURL=155.a14306c6.chunk.js.map
